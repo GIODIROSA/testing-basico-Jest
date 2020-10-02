a59b865bb9f7825416ce978a3ebc401902d5f1b0
giodirosa <giodirosaartes@gmail.com>
2020-10-02 12:55:27 -0300

    ultimos testing con snapshot

--- __test__/snapshot.test.js
+++ __test__/snapshot.test.js
@@ -5,6 +5,24 @@ describe("Es hora de las instaneas", () => {
   test("Snapshot", () => {
     expect(getCharacter(rick)).toMatchSnapshot();
   });
+
+  test("Siempre fallara la instantanea", () => {
+    const user = {
+      createAt: new Date(),
+      id: Math.floor(Math.random() * 20),
+    };
+    expect(user).toMatchSnapshot();
+  });
+
+  test("Tenemos una excepsion del codigo", () => {
+    const user = {
+      id: Math.floor(Math.random() * 20),
+      name: "Oscar Barajas",
+    };
+    expect(user).toMatchSnapshot({
+      id: expect.any(Number),
+    });
+  });
 });
 
 //para actualizar el snapshot... usar el comando jest -u
